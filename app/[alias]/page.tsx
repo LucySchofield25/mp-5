847cfcefcf780c360936ba626a9fbb223b30eea2
lucy schofield <scholucy@bu.edu>
2025-04-16 11:57:57 -0400

    fixed params error in page.tsx

--- app/[alias]/page.tsx
+++ app/[alias]/page.tsx
@@ -2,12 +2,14 @@ import { redirect } from 'next/navigation';
 import getCollection, { URLS_COLLECTION } from '@/lib/db';
 import { Url } from '@/lib/model/Url';
 
-export default async function Page({ params }: { params: { alias: string } }) {
+export default async function Page(props: { params: { alias: string } }) {
+    const { alias } = props.params;
+
     const collection = await getCollection<Url>(URLS_COLLECTION);
-    const record = await collection.findOne({ alias: params.alias });
+    const record = await collection.findOne({ alias });
 
     if (!record) {
-        return <p>Alias not found</p>;
+        return <p className="text-red-600 text-center mt-10">Alias not found</p>;
     }
 
     redirect(record.url);
