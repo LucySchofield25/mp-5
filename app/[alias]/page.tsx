0cfd93e9f103ae23f1f9a922ab6a192bfb72d2ec
lucy schofield <scholucy@bu.edu>
2025-04-18 12:30:26 -0400

    debugging, added const dynamic

--- app/[alias]/page.tsx
+++ app/[alias]/page.tsx
@@ -2,9 +2,10 @@ import { redirect } from 'next/navigation';
 import getCollection, { URLS_COLLECTION } from '@/lib/db';
 import { Url } from '@/lib/model/Url';
 
-export default async function Page(props: { params: { alias: string } }) {
-    const { alias } = props.params;
+export const dynamic = 'force-dynamic';
 
+export default async function Page({params}: {params: Promise<{alias: string}>}) {
+    const { alias } = await params;
     const collection = await getCollection<Url>(URLS_COLLECTION);
     const record = await collection.findOne({ alias });
 
